c4acf98715757cfa7182a97539b467dce7b2162d
André Oliveira <p32blo@gmail.com>
2016-06-02 14:05:22 +0100

    Implement add method for Images
    - Implement a add method to add Image values
    - Refactoring the metadata reading used in open and add methods

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::fs::File;
 use std::io;
 use std::io::{Read, Error, ErrorKind};
 
+
+#[derive(Default)]
 struct Image {
 	iters : usize,
 	size_x : usize,
@@ -20,10 +22,27 @@ fn main() {
 
 	let _exec = args.next();
 
-	for arg in args {
+	let mut image = Image::new();
 
-
+	for arg in &mut args {
 		match Image::open(&arg) {
+			Ok(img) => {
+				image = img;
+				println!("Load image {:?}...", arg);
+				break;
+			},
+			Err(e) => {
+				match e.kind() {
+					ErrorKind::InvalidData =>
+						println!("warn: file {:?} is not a netpbm file", arg),
+					_ => println!("warn: file {:?} does not exist", arg)
+				}
+			}
+		}
+	}
+
+	for arg in args {
+		match image.add(&arg) {
 			Ok(_) => println!("Load image {:?}...", arg),
 			Err(e) => {
 				match e.kind() {
@@ -33,60 +52,105 @@ fn main() {
 			}
 		}
 	}
+
+
 }
 
 impl Image {
+
+	fn new() -> Self { Image::default() }
+
 	fn open(filename: &str) -> Result<Self, io::Error>
+	{
+		let (mut image, content) = try!(Self::read_metadata(filename));
+
+		let mut split = content.split_whitespace();
+
+		let _metadata = split.nth(4);
+
+		let img_size = image.size_x * image.size_y;
+		let img_rgb_size = img_size * 3;
+
+		let mut img = vec![0; img_rgb_size];
+
+		for (i, word) in split.enumerate() {
+			let val: usize = word.parse().unwrap();
+			img[i] = val * image.iters;
+	//		println!("debug: {}, {:?}, {}", i, word, img[i]);
+		}
+
+		println!("debug: vec_size = {} / {}", img.len(), img_rgb_size);
+
+		image.data = img;
+
+		Ok(image)
+	}
+
+	fn read_metadata(filename: &str) -> Result<(Self, String), io::Error>
 	{
 		let mut file = try!(File::open(filename));
 
 		let mut content = String::new();
 		try!(file.read_to_string(&mut content));
 
-		let mut split = content.split_whitespace();
+		let iters: usize;
+		let size_x: usize;
+		let size_y: usize;
+		let max_val: usize;
 
-		if let Some(val) = split.next() {
-			if val != "P3" {
-				return Err(Error::new(ErrorKind::InvalidData, "File does not contain 'P3' tag"));
+		{
+			let mut split = content.split_whitespace();
+
+			if let Some(val) = split.next() {
+				if val != "P3" {
+					return Err(Error::new(ErrorKind::InvalidData, "File does not contain 'P3' tag"));
+				}
 			}
+
+			let (hash, number) = split.next().unwrap().split_at(1);
+
+			iters = number.parse().unwrap();
+
+			if hash != "#" {
+				return Err(Error::new(ErrorKind::InvalidData, "File does not have required metadata"));
+			}
+
+			size_x = split.next().unwrap().parse().unwrap();
+			size_y = split.next().unwrap().parse().unwrap();
+			max_val = split.next().unwrap().parse().unwrap();
+
+			println!("debug: iters = {:?}, {:?}", hash, iters);
+			println!("debug: size_x = {:?}", size_x);
+			println!("debug: size_y = {:?}", size_y);
+			println!("debug: max_val = {:?}", max_val);
 		}
 
-		let (hash, number) = split.next().unwrap().split_at(1);
-
-		let iters: usize = number.parse().unwrap();
-
-		if hash != "#" {
-			return Err(Error::new(ErrorKind::InvalidData, "File does not have required metadata"));
-		}
-
-		let size_x: usize = split.next().unwrap().parse().unwrap();
-		let size_y: usize = split.next().unwrap().parse().unwrap();
-		let max_val: usize = split.next().unwrap().parse().unwrap();
-
-		println!("debug: iters = {:?}, {:?}", hash, iters);
-		println!("debug: size_x = {:?}", size_x);
-		println!("debug: size_y = {:?}", size_y);
-		println!("debug: max_val = {:?}", max_val);
-
-		let img_size = size_x * size_y;
-		let img_rgb_size = img_size * 3;
-
-		let mut img = vec![0; img_rgb_size];
-
-		for (i, word) in split.enumerate() {
-			img[i] = word.parse().unwrap();
-	//		println!("debug: {}, {:?}, {}", i, word, img[i]);
-		}
-
-		println!("debug: vec_size = {}", img.len());
-
-		Ok(Image {
+		Ok((Image {
 			iters: iters,
 			size_x: size_x,
 			size_y: size_y,
 			max_val: max_val,
-			data: img
-		})
+			data: Vec::default()
+		}, content))
+	}
+
+	fn add(&mut self, filename: &str) -> Result<(), io::Error>
+	{
+		let (image, content) = try!(Self::read_metadata(filename));
+
+		let mut split = content.split_whitespace();
+
+		let _metadata = split.nth(4);
+
+		println!("test: {:?}", _metadata);
+
+		for (i, word) in split.enumerate() {
+			let val: usize = word.parse().unwrap();
+			self.data[i] += val * image.iters;
+			//println!("debug: {}, {:?}, {}", i, word, self.data[i]);
+		}
+
+		Ok(())
 	}
 }
 
